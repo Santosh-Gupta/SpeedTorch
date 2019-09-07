54d3c2d7a2ec836262d6def238ac5cd745a34e86
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-09-07 11:59:00 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import codecs
+from setuptools import setup, find_packages
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+    
+try:
+  import cupy
+except:
+    print('Cupy not installed. Package will install cupy, which will take several minutes')
+    print('If you would like to install Cupy yourself, check here https://docs-cupy.chainer.org/en/stable/install.html#install-cupy')
+    install_requires.append('cupy')
+
+setup(
+    name='SparseTorch',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/Santosh-Gupta/SparseTorch',
+    license='MIT',
+    author='Santosh Gupta',
+    author_email='SanGupta.ML@gmail.com',
+    description='SP',
+    long_description='None so far',
+    long_description_content_taype='text/markdown',
+    python_requires='>=3.5.0',
+    install_requires=install_requires,
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
