e40df07d8b522d26400f397af44e21a36affc515
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-09-11 18:16:37 -0700

    Update __init__.py

--- SpeedTorch/__init__.py
+++ SpeedTorch/__init__.py
@@ -7,6 +7,14 @@ from SpeedTorch.CUPYLive import OptimizerFactory
 from SpeedTorch.CUPYLive import COM
 from SpeedTorch.CUPYLive import DataGadget
 
+from SpeedTorch.CPUCupyPinned import PMemoryMM
+from SpeedTorch.CPUCupyPinned import my_pinned_allocatorMM
+from SpeedTorch.CPUCupyPinned import _CommonMM
+from SpeedTorch.CPUCupyPinned import ModelFactoryMM
+from SpeedTorch.CPUCupyPinned import OptimizerFactoryMM
+from SpeedTorch.CPUCupyPinned import COMMM
+from SpeedTorch.CPUCupyPinned import DataGadgetMM
+
 from SpeedTorch.GPUTorch import _GPUPytorchCommon
 from SpeedTorch.GPUTorch import GPUPytorchModelFactory
 from SpeedTorch.GPUTorch import GPUPytorchOptimizerFactory
