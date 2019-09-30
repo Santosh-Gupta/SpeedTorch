a7573ef53e68a740afb0cf81e9e6dafb2be9ec5b
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-09-29 18:15:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except:
 
 setup(
     name='SpeedTorch',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     url='https://github.com/Santosh-Gupta/SpeedTorch',
     license='MIT',
